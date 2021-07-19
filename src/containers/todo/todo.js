8a6bfb60b358c60b00e737fa63db3c3f6e88bd0b
unknown <vazgen.py@gmail.com>
2021-07-19 16:33:43 +0400

    'feat(START):_project_started'

---
+++ src/containers/todo/todo.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import styles from './todo';
+
+class Todo extends Component {
+    render() {
+        return (
+                <h3>Todo</h3>
+            )
+    }
+}
+
+export default Todo;
